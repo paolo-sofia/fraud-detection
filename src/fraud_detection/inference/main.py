9009c1c1e62d43315bfd01fa60ba14bc3bfc341f
Paolo Sofia <paolosofia6@gmail.com>
2024-03-15 13:04:30 +0100

    refactor: remove useless logging

--- src/fraud_detection/inference/main.py
+++ src/fraud_detection/inference/main.py
@@ -23,17 +23,13 @@ async def predict() -> dict[str, str]:
 
 @app.post("/predict")
 async def predict(data: dict[str, str | int | bool | float]) -> dict[str, str | dict[str, int | float]]:
-    logging.error("PORCODIO\n\n\n\n\n")
-    logging.error(f"data: {data}")
     try:
         data: pd.DataFrame = prepare_data_for_inference(data, columns).to_pandas()
-        logging.error(f"data: {data.shape}")
+
         prediction_probability: np.ndarray = model.predict_proba(data)[0]
-        logging.error(f"prediction_probability: {prediction_probability}")
+
         prediction: bool = bool(prediction_probability[1] > float(os.getenv("THRESHOLD", "0.5")))
-        logging.error(f"prediction: {prediction}")
         probability: float = float(prediction_probability[1]) if prediction else float(prediction_probability[0])
-        logging.error(f"probability: {probability}")
 
         return {"message": "Prediction successfully", "data": {"class": prediction, "probability": probability}}
 
